207024655bbb8bac35e98a9902078c6564de4d84
Sofia Montoya Bolivar <sofia.montoya@sequal.com.co>
2022-09-14 18:50:55 -0500

    Conexión templates-urls-views de enfermera y jefe

--- MediPlanner/Enfermeras/views.py
+++ MediPlanner/Enfermeras/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+# from Enfermeras.models import Enfermeras PARA CUANDO SE CREE LA BASE DE DATOS
 
 # Create your views here.
+def enfermeras(request):
+    # PARA CUANDO SE CREE LA BASE DE DATOS enfermeras = Enfermeras.objects.all()
+    return render(request, "Enfermeras/index-nurse.html")
